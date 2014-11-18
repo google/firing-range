d181c325fa20e26a9c1d605fc694327f78db3ac2
Claudio Criscione <claudioc@google.com>
2014-11-18 08:32:16 -0800

    Add to the opensource release the reverse clickjacking servlet and the utils package. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=80205305

---
+++ src/utils/Responses.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2014 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.testing.security.firingrange.utils;
+
+import com.google.common.base.Preconditions;
+import com.google.common.net.HttpHeaders;
+
+import java.io.IOException;
+
+import javax.annotation.concurrent.Immutable;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Utility class handling HTTP responses.
+ */
+@Immutable
+public final class Responses {
+
+  private Responses() {}
+
+  /**
+   * Sends a "normal" response, with all the standard headers. 
+   */
+  public static void sendNormalPage(HttpServletResponse response, String body) throws IOException {
+    response.setHeader(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate");
+    response.setHeader(HttpHeaders.PRAGMA, "no-cache");
+    response.setDateHeader(HttpHeaders.EXPIRES, 0);
+    response.setHeader(HttpHeaders.CONTENT_TYPE, "text/html; charset=utf-8");
+    response.setStatus(200);
+    response.getWriter().write(body);
+  }
+
+  /**
+   * Sends an XSS response. 
+   */
+  public static void sendXssed(HttpServletResponse response, String body) throws IOException {
+    sendXssed(response, body, "text/html; charset=utf-8");
+  }
+
+  /**
+   * Sends an XSS response of a given type. 
+   */
+  public static void sendXssed(HttpServletResponse response, String body, String contentType)
+      throws IOException {
+    response.setHeader(HttpHeaders.X_XSS_PROTECTION, "0");
+    response.setHeader(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate");
+    response.setHeader(HttpHeaders.PRAGMA, "no-cache");
+    response.setDateHeader(HttpHeaders.EXPIRES, 0);
+    response.setHeader(HttpHeaders.CONTENT_TYPE, contentType);
+    response.setStatus(200);
+    response.getWriter().write(body);
+  }
+
+  /**
+   * Sends an error to the user with the given {@code status} and body.
+   */
+  public static void sendError(HttpServletResponse response, String body, int status)
+      throws IOException {
+    Preconditions.checkArgument(status > 300);
+    response.setStatus(status);
+    response.getWriter().write(body);
+  }
+
+  /**
+   * Sends a redirect to the user.
+   */
+  public static void sendRedirect(HttpServletResponse response, String location) {
+    response.setStatus(302);
+    response.setHeader(HttpHeaders.LOCATION, location);
+  }
+}
---
+++ src/utils/Templates.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2014 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.testing.security.firingrange.utils;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Splitter;
+import com.google.common.io.Resources;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Logger;
+
+import javax.annotation.concurrent.Immutable;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * A helper class to manipulate templates.
+ */
+@Immutable
+public final class Templates {
+  private static final String ERROR_TEMPLATE = "data/error.tmpl";
+  @VisibleForTesting
+  static final String PAYLOAD_PLACEHOLDER = "%%PAYLOAD%%";
+  private static final Logger logger = Logger.getLogger(Templates.class.getCanonicalName());
+
+  private Templates() {}
+
+  /**
+   * Extract a template given an HTTP request. The last path component is the template.
+   * @throws IllegalArgumentException if it cannot find the template.
+   */
+  public static String getTemplate(HttpServletRequest request, Class<? extends HttpServlet> clazz)
+      throws IllegalArgumentException {
+    String firstPathPart = Splitter.on('/').splitToList(request.getPathInfo()).get(1);
+    String templateName = firstPathPart + ".tmpl";
+    return getTemplate(templateName, clazz);
+  }
+
+  /**
+   * Extracts a template given the path relative to a {@code clazz}.
+   * @throws IllegalArgumentException if it cannot find the template.
+   */
+  public static String getTemplate(String relativePath, Class<? extends HttpServlet> clazz) 
+      throws IllegalArgumentException {
+    Preconditions.checkArgument(!relativePath.contains(".."));
+    try {
+      URL resource = Resources.getResource(clazz, "data/" + relativePath);
+      return templateToString(resource);
+    } catch (IllegalArgumentException e) {
+      logger.info("Cannot find template for " + relativePath);
+      throw new IllegalArgumentException(errorTemplate());
+    }
+  }
+
+  @VisibleForTesting
+  static String errorTemplate() {
+    // Unable to find this template. Return error template.
+    try {
+      return templateToString(Resources.getResource(Templates.class, ERROR_TEMPLATE));
+    } catch (IllegalArgumentException e2) {
+      // Unable to find the error template!
+      logger.severe("Cannot find error template");
+      return "";
+    }
+  }
+
+  private static String templateToString(URL resource) {
+    try {
+      return Resources.toString(resource, UTF_8);
+    } catch (IOException e) {
+      logger.severe("Cannot open template: " + resource);
+      return "ERROR, cannot open template";
+    }
+  }
+
+  /**
+   * Sets the payload in a given template.
+   */
+  public static String replacePayload(String template, String payload) {
+    return template.replace(PAYLOAD_PLACEHOLDER, payload);
+  }
+}
